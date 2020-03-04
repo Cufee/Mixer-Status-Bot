b1e146e9e9f2b8f0a6c3bbfc613c8430c5e582d9
Cufee <vkouzin@outlook.com>
2020-03-04 04:10:09 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,6 @@ mode = settings["mode"]
 prefix = settings["prefix"]
 default_game = settings["default_game"]
 client = commands.Bot(command_prefix = prefix, case_insensitive=True)
-client.remove_command('help')
 
 
 #Startup
@@ -32,11 +31,14 @@ async def on_ready():
 
 
 #Root Commands
-@client.command
-async def help(ctx):
-    help_cmd = f'```Use {prefix}status to check the current status of Mixer.com.\n\nParameters available: all, web, video, vod, xbox, api.\n\nStatus updated every 10 minutes.```'
-    await ctx.send(help_cmd)
- 
+@client.command(hidden=True)
+@commands.is_owner()
+async def startupdate(ctx):
+    print('Starting update')
+    with open(f'{os.path.dirname(os.path.realpath(__file__))}/cogs/mixer_status/img/update.jpg', 'rb') as img:
+        await client.user.edit(avatar=img.read())
+    client.close()
+
 
 #Cog managment
 @client.command(hidden=True)
